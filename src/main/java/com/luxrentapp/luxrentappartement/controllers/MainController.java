2b7de04afdb7eda7f72135cd0c5e109a48ca5ab1
gillespatrick <ngondogillespatrick@gmail.com>
2022-01-21 01:01:23 -0500

    Project init

---
+++ src/main/java/com/luxrentapp/luxrentappartement/controllers/MainController.java
@@ -0,0 +1,11 @@
+package com.luxrentapp.luxrentappartement.controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainController extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.luxrentapp.luxrentappartement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.luxrentapp.luxrentappartement to javafx.fxml;
+    exports com.luxrentapp.luxrentappartement;
+}
